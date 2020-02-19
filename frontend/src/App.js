7ef86e07f219dd5ae31b70f37378411d3c143fad
jiajie <jiajie@momenta.ai>
2020-02-19 18:46:10 +0800

    update jilin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ require('echarts/map/js/province/hebei.js');
 require('echarts/map/js/province/shanxi.js');
 require('echarts/map/js/province/neimenggu.js');
 require('echarts/map/js/province/liaoning.js');
+require('echarts/map/js/province/jilin.js');
 require('echarts/map/js/province/fujian.js');
 require('echarts/map/js/province/guangdong.js');
 
@@ -55,7 +56,7 @@ class Chart extends Component {
             },
             series: [
                 {
-                    name: '确诊',
+                    name: '累计确诊',
                     type: 'map',
                     mapType: null,
                     label: {
@@ -103,7 +104,7 @@ class App extends Component {
             { title: '山西', key: 'shanxi', disabled: true, data: {} },
             { title: '内蒙古', key: 'neimenggu' , disabled: false, data: {} },
             { title: '辽宁', key: 'liaoning' , disabled: false, data: {} },
-            { title: '吉林', key: 'jilin' , disabled: true, data: {} },
+            { title: '吉林', key: 'jilin' , disabled: false, data: {} },
             { title: '黑龙江', key: 'heilongjiang' , disabled: true, data: {} },
             { title: '上海', key: 'shanghai' , disabled: true, data: {} },
             { title: '江苏', key: 'jiangsu' , disabled: true, data: {} },
