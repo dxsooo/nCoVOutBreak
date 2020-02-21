6a5b9b18c957ddf9034a7babe3a1c6914a6cb011
jiajie <jiajie@momenta.ai>
2020-02-21 20:14:24 +0800

    decrease bundle size

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,11 @@
 import React, {Component} from 'react';
-import ReactEcharts from 'echarts-for-react'
+import ReactEchartsCore from 'echarts-for-react/lib/core'
+import echarts from 'echarts/lib/echarts'
+import 'echarts/lib/chart/map';
+import 'echarts/lib/component/title';
+import 'echarts/lib/component/tooltip';
+import 'echarts/lib/component/visualMap';
+import 'echarts/lib/component/toolbox';
 import axios from 'axios';
 import './App.css';
 import {Tabs} from 'antd';
@@ -9,7 +15,6 @@ const {TabPane} = Tabs;
 require('echarts/map/js/province/beijing.js');
 require('echarts/map/js/province/tianjin.js');
 require('echarts/map/js/province/hebei.js');
-require('echarts/map/js/province/shanxi.js');
 require('echarts/map/js/province/neimenggu.js');
 require('echarts/map/js/province/liaoning.js');
 require('echarts/map/js/province/jilin.js');
@@ -24,7 +29,7 @@ class Chart extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            option:this.getInitialOption(),
+            option: this.getInitialOption(),
         };
     }
 
@@ -90,7 +95,8 @@ class Chart extends Component {
                 return {'name': x.city, 'value': x.confirmed}
             });
             return (
-                <ReactEcharts
+                <ReactEchartsCore
+                    echarts={echarts}
                     style={{height: '700px', width: '100%'}}
                     notMerge={true}
                     lazyUpdate={true}
@@ -105,40 +111,40 @@ class App extends Component {
     constructor(props) {
         super(props);
         const panes = [
-            { title: '北京', key: 'beijing', disabled: false, data: {} },
-            { title: '天津', key: 'tianjin', disabled: false, data: {} },
-            { title: '河北', key: 'hebei' , disabled: false, data: {} },
-            { title: '山西', key: 'shanxi', disabled: true, data: {} },
-            { title: '内蒙古', key: 'neimenggu' , disabled: false, data: {} },
-            { title: '辽宁', key: 'liaoning' , disabled: false, data: {} },
-            { title: '吉林', key: 'jilin' , disabled: false, data: {} },
-            { title: '黑龙江', key: 'heilongjiang' , disabled: false, data: {} },
-            { title: '上海', key: 'shanghai' , disabled: true, data: {} },
-            { title: '江苏', key: 'jiangsu' , disabled: false, data: {} },
-            { title: '浙江', key: 'zhejiang' , disabled: false, data: {} },
-            { title: '安徽', key: 'anhui' , disabled: true, data: {} },
-            { title: '福建', key: 'fujian' , disabled: false, data: {} },
-            { title: '江西', key: 'jiangxi' , disabled: true, data: {} },
-            { title: '山东', key: 'shandong' , disabled: true, data: {} },
-            { title: '河南', key: 'henan' , disabled: true, data: {} },
-            { title: '湖北', key: 'hubei' , disabled: true, data: {} },
-            { title: '湖南', key: 'hunan' , disabled: true, data: {} },
-            { title: '广东', key: 'guangdong' , disabled: false, data: {} },
-            { title: '广西', key: 'guangxi' , disabled: true, data: {} },
-            { title: '海南', key: 'hainan' , disabled: true, data: {} },
-            { title: '重庆', key: 'chongqing' , disabled: true, data: {} },
-            { title: '四川', key: 'sichuan' , disabled: true, data: {} },
-            { title: '贵州', key: 'guizhou' , disabled: true, data: {} },
-            { title: '云南', key: 'yunnan' , disabled: true, data: {} },
-            { title: '西藏', key: 'xizang' , disabled: false, data: {} },
-            { title: '陕西', key: 'shaanxi' , disabled: true, data: {} },
-            { title: '甘肃', key: 'gansu' , disabled: true, data: {} },
-            { title: '青海', key: 'qinghai' , disabled: true, data: {} },
-            { title: '宁夏', key: 'ningxia' , disabled: true, data: {} },
-            { title: '新疆', key: 'xinjiang' , disabled: true, data: {} },
-            { title: '香港', key: 'hongkong' , disabled: true, data: {} },
-            { title: '澳门', key: 'macao' , disabled: true, data: {} },
-            { title: '台湾', key: 'taiwan' , disabled: true, data: {} },
+            {title: '北京', key: 'beijing', disabled: false, data: {}},
+            {title: '天津', key: 'tianjin', disabled: false, data: {}},
+            {title: '河北', key: 'hebei', disabled: false, data: {}},
+            {title: '山西', key: 'shanxi', disabled: true, data: {}},
+            {title: '内蒙古', key: 'neimenggu', disabled: false, data: {}},
+            {title: '辽宁', key: 'liaoning', disabled: false, data: {}},
+            {title: '吉林', key: 'jilin', disabled: false, data: {}},
+            {title: '黑龙江', key: 'heilongjiang', disabled: false, data: {}},
+            {title: '上海', key: 'shanghai', disabled: true, data: {}},
+            {title: '江苏', key: 'jiangsu', disabled: false, data: {}},
+            {title: '浙江', key: 'zhejiang', disabled: false, data: {}},
+            {title: '安徽', key: 'anhui', disabled: true, data: {}},
+            {title: '福建', key: 'fujian', disabled: false, data: {}},
+            {title: '江西', key: 'jiangxi', disabled: true, data: {}},
+            {title: '山东', key: 'shandong', disabled: true, data: {}},
+            {title: '河南', key: 'henan', disabled: true, data: {}},
+            {title: '湖北', key: 'hubei', disabled: true, data: {}},
+            {title: '湖南', key: 'hunan', disabled: true, data: {}},
+            {title: '广东', key: 'guangdong', disabled: false, data: {}},
+            {title: '广西', key: 'guangxi', disabled: true, data: {}},
+            {title: '海南', key: 'hainan', disabled: true, data: {}},
+            {title: '重庆', key: 'chongqing', disabled: true, data: {}},
+            {title: '四川', key: 'sichuan', disabled: true, data: {}},
+            {title: '贵州', key: 'guizhou', disabled: true, data: {}},
+            {title: '云南', key: 'yunnan', disabled: true, data: {}},
+            {title: '西藏', key: 'xizang', disabled: false, data: {}},
+            {title: '陕西', key: 'shaanxi', disabled: true, data: {}},
+            {title: '甘肃', key: 'gansu', disabled: true, data: {}},
+            {title: '青海', key: 'qinghai', disabled: true, data: {}},
+            {title: '宁夏', key: 'ningxia', disabled: true, data: {}},
+            {title: '新疆', key: 'xinjiang', disabled: true, data: {}},
+            {title: '香港', key: 'hongkong', disabled: true, data: {}},
+            {title: '澳门', key: 'macao', disabled: true, data: {}},
+            {title: '台湾', key: 'taiwan', disabled: true, data: {}},
         ];
         this.state = {
             activeKey: this.getDefaultKey(panes),
@@ -149,7 +155,7 @@ class App extends Component {
 
     getDefaultKey(panes) {
         const province = /(.*?)[省市特自]/.exec(window["returnCitySN"].cname)[1];
-        const m = panes.filter(x=>{
+        const m = panes.filter(x => {
             return x.title === province && !x.disabled;
         });
         if (m.length > 0)
@@ -161,13 +167,13 @@ class App extends Component {
         axios.get("https://service-r8373tyc-1253891892.gz.apigw.tencentcs.com/api/v1/provinces/" + key).then(
             result => {
                 const panes = this.state.panes;
-                const panef = panes.filter(x=>{
+                const panef = panes.filter(x => {
                     return x.key === key;
                 });
                 panes[panes.indexOf(panef[0])].data = result.data;
                 this.setState({
-                    panes:panes,
-                    loaded:true,
+                    panes: panes,
+                    loaded: true,
                 })
             },
             error => {
@@ -181,7 +187,7 @@ class App extends Component {
     }
 
     onChange = activeKey => {
-        this.setState({ activeKey, loaded:false });
+        this.setState({activeKey, loaded: false});
         this.updateData(activeKey);
     };
 
@@ -193,7 +199,7 @@ class App extends Component {
                     activeKey={this.state.activeKey}
                 >
                     {this.state.panes.map(pane => (
-                        <TabPane tab={pane.title} key={pane.key} disabled={pane.disabled} >
+                        <TabPane tab={pane.title} key={pane.key} disabled={pane.disabled}>
                             <Chart data={pane.data} loaded={this.state.loaded}/>
                         </TabPane>
                     ))}
